bff531c1e4a311c409f6287bd2fb562ca107e7ef
Su-p-riya <119415185+Su-p-riya@users.noreply.github.com>
2026-05-15 15:50:30 +0530

    Create app.py

---
+++ Tools/PQCheck/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+from crypto import crypto_bp
+from dependency import dependency_bp
+from ai_chat import ai_chat_bp
+
+app = Flask(__name__)
+
+app.register_blueprint(crypto_bp)
+app.register_blueprint(dependency_bp)
+app.register_blueprint(ai_chat_bp)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
